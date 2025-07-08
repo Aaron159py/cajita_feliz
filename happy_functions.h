463bd668088b3530e7685bd5a34f04dd17ba61d0
Aaron Ascencio <aaron.ascencio@ug.uchile.cl>
2025-07-08 10:46:17 -0400

    function of tare

--- happy_functions.h
+++ happy_functions.h
@@ -8,6 +8,24 @@ void showWeight(){
 
 
 void tareScale(){
-	scale.tare
+	if (scale.is_ready()){
+		scale.set_scale();
+		Serial.println("Taring, please remove any objects from the scale");
+		lcd.print("Taring, remove weights");
 
+		delay(5000);
+		scale.tare();
+		Serial.println("tare done");
+		Serial.println("Put a known weight on the scale");
+		delay(5000);
+
+		long reading = scale.get_units(10);
+		Serial.print("Result ");
+		lcd.print("Result ");
+		Serial.println(reading);
+		lcd.print(reading,3);
+	}
+	else {
+		Serial.println("404 HX711 not found");
+		lcd.print("404 not found");
 }
